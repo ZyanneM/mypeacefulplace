43391d97df58ec2f575c050a4a31624c26289c76
ZyanneM <92530551+ZyanneM@users.noreply.github.com>
2023-03-17 15:22:28 +0100

    try to compare width and height img

--- src/components/QuoteGallery/QuoteGallery.jsx
+++ src/components/QuoteGallery/QuoteGallery.jsx
@@ -2,23 +2,42 @@ import LoginBtn from "../LoginBtn/LoginBtn";
 import Navbar from "../Navbar/Navbar";
 import styles from "./QuoteGallery.module.css";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function QuoteGallery({ quoteCards }) {
+
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  const [width, setWidth] = useState(null);
+  const [height, setHeight] = useState(null);
+
   const imgElements = quoteCards.map((quoteCard) => {
+
+
+  function handleImageLoad(event) {
+    var width = event.target.naturalWidth;
+    var height = event.target.naturalHeight;
+    console.log(`Image ${quoteCard.id} : width = ${width}, height = ${height}`);
+    setWidth(width);
+    setHeight(height);
+    console.log(width);
+    setIsImageLoaded(true);
+  }
     return (
-      <li className={styles.quoteGalleryLi}>
+      <div className={styles.quoteGalleryLi}>
         <img
-          className={styles.quoteView}
+          className={`${styles.quoteView} ${width>height ? styles.landscape : ''}`}
           key={quoteCard.id}
           src={`quoteviews/${quoteCard.image}`}
           alt={quoteCard.image}
+          onLoad={handleImageLoad}
         />
-      </li>
+      </div>
     );
   });
 
   return (
-    <>
+    <div>
       {/* Ici la barre de recherche etc. */}
       <div className={styles.quoteTopMenu}>
       <Link href="/signup" key="signupLink">
@@ -26,8 +45,10 @@ export default function QuoteGallery({ quoteCards }) {
       </Link>
       <input className={styles.quoteSearchBar} type="text" placeholder="Rechercher une citation"></input>
       </div>
-      <ul className={styles.quoteGallery}>{imgElements}</ul>
+      <div className={styles.quoteGallery}>{imgElements}</div>
       <Navbar />
-    </>
+    </div>
   );
 }
+
+// */${isImageLoaded == true && newImgWidth > newImgHeight ? styles.landscape : ''}*/`
